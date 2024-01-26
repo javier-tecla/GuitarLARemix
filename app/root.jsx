414be5c79a9203fbaf719900d6b0cfdf8fc64e15
javierborjas <cristman11@gmail.com>
2024-01-26 20:21:14 -0300

    Agregar Hojas de Estilos Externas

--- app/root.jsx
+++ app/root.jsx
@@ -1,32 +1,66 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+    Meta,
+    Links
+} from '@remix-run/react'
+import styles from './styles/index.css'
 
-export const links = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+export function meta() {
+    return  [ 
+        {
+            charset: 'utf-8',
+            title: 'GuitarLA - Remix',
+            viewport: 'width=device-width,initial-scale=1'
+        }
+    ]
+}
+
+
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com',
+        },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com',
+            crossOrigin: 'true'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap'
+        },
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
+
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+
+    return(
+        <Document>
+            <h1>Hola Mundo</h1>
+        </Document>
+    )
+}
+
+function Document({children}) {
+    return (
+        <html lang="es">
+            <head>
+                <Meta/>
+                <Links/>
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
 }

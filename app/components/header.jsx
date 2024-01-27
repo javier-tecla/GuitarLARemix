7cc4c3f468e0cb21540679f9397f23d7b3980435
javierborjas <cristman11@gmail.com>
2024-01-26 23:40:30 -0300

    Css al Header y Resaltar la pagina actual

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,7 +1,10 @@
-import { Link } from "@remix-run/react"
+import { Link, useLocation } from "@remix-run/react"
 import logo from '../../public/img/logo.svg'
 
 function Header() {
+
+    const location = useLocation()
+    console.log(location)
   return (
     <header className="header">
         <div className="contenedor barra">
@@ -12,15 +15,19 @@ function Header() {
             <nav className="navegacion">
                 <Link
                     to="/"
+                    className={location.pathname === '/' ? 'active' : '' }
                 >Inicio</Link>
                  <Link
                     to="/nosotros"
+                    className={location.pathname === '/nosotros' ? 'active' : '' }
                 >Nosotros</Link>
                   <Link
                     to="/tienda"
+                    className={location.pathname === '/tienda' ? 'active' : '' }
                 >Tienda</Link>
                   <Link
                     to="/blog"
+                    className={location.pathname === '/blog' ? 'active' : '' }
                 >Blog</Link>
             </nav>
         </div>

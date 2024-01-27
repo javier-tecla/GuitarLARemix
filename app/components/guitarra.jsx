9eec09e61371ea4407b370bf196a62ddc5e4ee96
javierborjas <cristman11@gmail.com>
2024-01-27 20:35:57 -0300

    Mostrando la informacion de la guitarra

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -1,10 +1,23 @@
-
+import { Link } from "@remix-run/react";
 
 function Guitarra({guitarra}) {
 
-  console.log(guitarra)
+
+  const { descripcion, imagen, precio, url, nombre } = guitarra
+
+  const descripcionText = descripcion && descripcion[0]?.children[0]?.text || '';
+  
   return (
-    <div>guitarra</div>
+    <div className="guitarra">
+      <img src={imagen.data.attributes.formats.medium.url} alt={`Imagen guitarra ${nombre}`} />
+      <div className="contenido">
+          <h3>{nombre}</h3>
+          <p className="descripcion">{descripcionText}</p>
+          <p className="precio">${precio}</p>
+
+          <Link className="enlace" to={`/guitarras/${url}`}>Ver Producto</Link>
+      </div>
+    </div>
   )
 }
 

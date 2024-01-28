73b41885a13f7fcae6cc19757bb11b45bf03fcf2
javierborjas <cristman11@gmail.com>
2024-01-27 21:15:55 -0300

    Aplicando Css al listado de Guitarras

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,22 @@
 import { useLoaderData }from '@remix-run/react'
 import { getGuitarras } from '~/models/guitarras.server'
 import Guitarra from '~/components/guitarra'
+import styles from '~/styles/guitarras.css'
+
+export function meta() {
+  return {
+    title: 'GuitarLA - Tienda de Guitarras',
+    description: 'GuitarLA - Nuesta colección de guitarras'
+  }
+}
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 
 export async function loader() {
   const guitarras = await getGuitarras()

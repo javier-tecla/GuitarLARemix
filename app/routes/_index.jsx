b148f8d773eab7ab7b3e998f05cacff508a2fa4f
javierborjas <cristman11@gmail.com>
2024-02-05 21:06:34 -0300

    Llamar el Contenido para el curso y aplicar Css con jsx

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,13 +1,23 @@
 import { useLoaderData} from '@remix-run/react'
 import { getGuitarras } from "~/models/guitarras.server"
 import { getPosts } from "~/models/posts.server"
+import { getCurso } from "~/models/curso.server"
 import ListadoGuitarras from '~/components/listado-guitarras'
 import ListadoPosts from '~/components/listado-posts'
+import Curso from '~/components/curso'
 import stylesGuitarras from '~/styles/guitarras.css'
 import stylesPosts from '~/styles/blog.css'
+import stylesCurso from '~/styles/curso.css'
 
 export function meta() {
-
+  return [
+      
+    {title: 'GuitarLA -Remix'},
+    {
+      name: "description",
+      content: 'Venta de guitarras'
+    }
+  ]
 }
 export function links() {
   return [
@@ -18,6 +28,10 @@ export function links() {
     {
       rel: 'stylesheet',
       href: stylesPosts
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesCurso
     }
   ]
 
@@ -25,19 +39,22 @@ export function links() {
 export async function loader() {
 
   
-  const [guitarras, posts] = await Promise.all([
+  const [guitarras, posts, curso] = await Promise.all([
     getGuitarras(),
-    getPosts()
+    getPosts(),
+    getCurso()
   ])
+
   return {
     guitarras: guitarras.data,
-    posts: posts.data
+    posts: posts.data,
+    curso: curso.data
   }
 }
 
 function Index() {
 
-  const {guitarras, posts} = useLoaderData();
+  const {guitarras, posts, curso} = useLoaderData();
 
   
   return (
@@ -47,6 +64,10 @@ function Index() {
                guitarras={guitarras}
             />
       </main>
+
+      <Curso 
+          curso={curso.attributes}
+      />
 
       <section className='contenedor'>
             <ListadoPosts 

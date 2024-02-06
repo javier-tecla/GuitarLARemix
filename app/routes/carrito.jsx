382756d3c649deb1b05971b7ea0f476a0831adef
javierborjas <cristman11@gmail.com>
2024-02-06 16:29:08 -0300

    Solucionar el problema de Hidratacion en Remix

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useOutletContext } from '@remix-run/react'
+import { ClientOnly } from 'remix-utils/client-only'
 import styles from '~/styles/carrito.css'
 
 export function links() {
@@ -32,61 +33,63 @@ function Carrito() {
     
 
   return (
-    <main className="contenedor">
-        <h1 className="heading">Carrito de Compras</h1>
+    <ClientOnly fallback={'cargando...'}>
+      {() => (
+      <main className="contenedor">
+          <h1 className="heading">Carrito de Compras</h1>
 
-        <div className="contenido">
-          <div className='carrito'>
-            <h2>Articulos</h2>
+          <div className="contenido">
+            <div className='carrito'>
+              <h2>Articulos</h2>
 
-            {carrito?.length === 0 ? 'Carrito Vacio' : (
-              carrito?.map( producto => (
-                <div key={ producto.id} className='producto'>
-                  <div>
-                      <img src={producto.imagen} alt={`Imagen del producto ${producto.nombre}`} />
+              {carrito?.length === 0 ? 'Carrito Vacio' : (
+                carrito?.map( producto => (
+                  <div key={ producto.id} className='producto'>
+                    <div>
+                        <img src={producto.imagen} alt={`Imagen del producto ${producto.nombre}`} />
+                    </div>
+
+                    <div>
+                      <p className='nombre'>{producto.nombre}</p>
+                      <p>Cantidad:</p>
+
+                      <select
+                          value={producto.cantidad}
+                          className='select'
+                          onChange={ e => actualizarCantidad({
+                              cantidad: +e.target.value,
+                              id: producto.id
+                          })}
+                      >
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                      </select>
+
+                      <p className='precio'>$ <span>{producto.precio}</span></p>
+                      <p className='subtotal'>Subtotal: $ <span>{producto.cantidad * producto.precio}</span></p>
+                    </div>
+
+                    <button
+                          type='button'
+                          className='btn_eliminar'
+                          onClick={() => eliminarGuitarra(producto.id) }
+                    >X</button>
                   </div>
+                ))
+              )}
+            </div>
 
-                  <div>
-                    <p className='nombre'>{producto.nombre}</p>
-                    <p>Cantidad:</p>
-
-                    <select
-                        value={producto.cantidad}
-                        className='select'
-                        onChange={ e => actualizarCantidad({
-                            cantidad: +e.target.value,
-                            id: producto.id
-                        })}
-                    >
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                    </select>
-
-                    <p className='precio'>$ <span>{producto.precio}</span></p>
-                    <p className='subtotal'>Subtotal: $ <span>{producto.cantidad * producto.precio}</span></p>
-                  </div>
-
-                  <button
-                        type='button'
-                        className='btn_eliminar'
-                        onClick={() => eliminarGuitarra(producto.id) }
-                  >X</button>
-                </div>
-              ))
-            )}
-          </div>
-
-        <aside className="resumen">
-            <h3>Resumen del Pedido</h3>
-            <p>Total a pagar: ${total}</p>
-        </aside>
-        </div>
-
-        
-    </main>
+          <aside className="resumen">
+              <h3>Resumen del Pedido</h3>
+              <p>Total a pagar: ${total}</p>
+          </aside>
+          </div> 
+      </main>
+      )}
+    </ClientOnly>
   )
 }
 
